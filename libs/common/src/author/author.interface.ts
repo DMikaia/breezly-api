4de7af3aefa6d851f31a509bea72cb6cda1fdfd2
DMikaia <faeldan03@gmail.com>
2024-12-21 07:52:13 +0300

    config: removed unused author interface

--- libs/common/src/author/author.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthorUtils {
-  isAuthor(id: string, author_id: string): Promise<boolean>;
-}
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from './auth';
-export * from './author/author.interface';
 export * from './constants';
 export * from './database/database.module';
 export * from './global-filters';
